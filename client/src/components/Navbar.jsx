5cade6367524877deb65339a3e5ce517951c8c09
Ryan Lynn <ryanlynn@ryans-mbp.lan>
2023-09-27 22:18:01 -0400

    cleanup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setToken } from "./redux";
+import { useSelector } from "react-redux";
 
 /*
 Signed in
@@ -11,18 +9,12 @@ Signed in
 */
 export default function NavBar() {
     const token = useSelector((state) => state.auth.token);
-    const dispatch = useDispatch()
-console.log('token: ', token);
   return (
     <div className="nav">
       <Link to="/all-cards">Home</Link>
       <Link to="/all-cards">All Posts</Link>
       {!Boolean(token) && <Link to="/register">Register</Link>}
       {!Boolean(token) && <Link to="/login">Log In</Link>}
-      {Boolean(token) && <button  onClick={() => {
-        dispatch(setToken(null))
-        dispatch(setProfile(null))
-      }}>Log out</button>}
       <Link to="/user-profile">Profile</Link>
       <Link to="/new-post-form">New Post</Link>
     </div>
